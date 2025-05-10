fdd0cae942117cc0df733b9166d138066b89be96
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-05-10 13:51:04 -0300

    Ajustando o construtor de Bootcamp

--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -9,10 +9,17 @@ import java.util.Set;
 public class Bootcamp {
     private String nome;
     private String descricao;
-    private final LocalDate dataInicial = LocalDate.now();
-    private final LocalDate dataFinal = dataInicial.plusDays(45);
-    private Set<Dev> devsInscritos = new HashSet<>();
-    private Set<Conteudo> conteudos = new LinkedHashSet<>();
+    private final LocalDate dataInicial;
+    private final LocalDate dataFinal;
+    private Set<Dev> devsInscritos;
+    private Set<Conteudo> conteudos;
+
+    public Bootcamp(){
+        dataInicial = LocalDate.now();
+        dataFinal = dataInicial.plusDays(45);
+        devsInscritos = new HashSet<>();
+        conteudos = new LinkedHashSet<>();
+    }
 
 
     public String getNome() {
